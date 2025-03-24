48d4a1ab7c11f04cd1a14efd6f94b08a55017dca
CesarCaO <ccastelazo8@gmail.com>
2025-03-24 13:53:30 -0600

    Add frequency and total sum calculations to entropy results

--- apps/entropia/views.py
+++ apps/entropia/views.py
@@ -13,6 +13,9 @@ def calcular_entropia(texto):
     listRepeticionCaracteres = []
     listFrecuencias = []
     listLogaritmmos = []
+    listFrecueciaxLogaritmo=[]
+    frecuenciaTotal = 0.0
+    totalSumatoria = 0.0
 
     for nomcaracter in contarCadaLetra.keys():
         listCaracteres.append(nomcaracter)
@@ -20,18 +23,26 @@ def calcular_entropia(texto):
     for caracter in contarCadaLetra.values():#Recorre los valores de las letras
         listRepeticionCaracteres.append(caracter)
         frecuencia=caracter/Numletras #Calcula la frecuencia del caracter
+        frecuenciaTotal+=frecuencia
         listFrecuencias.append(frecuencia)
         #print(round(frecuencia,4)) #Imprime la frecuencia del caracter redondeada a 4 decimales
         logbase2=math.log(frecuencia,2)#Calcula el logaritmo en base 2 de la frecuencia
         listLogaritmmos.append(logbase2)
+        listFrecueciaxLogaritmo.append(frecuencia * logbase2)
         #print(round(logbase2,4))
         entropia+=-(frecuencia*logbase2)#frecuencia*logaritmo en base 2 de la frecuencia y luego se suma para sacar la entropia
+
+    for valor in listFrecueciaxLogaritmo:
+        totalSumatoria+=valor
     
-    tabla_frecuencias = list(zip(listCaracteres,listRepeticionCaracteres, listFrecuencias, listLogaritmmos))
+    tabla_frecuencias = list(zip(listCaracteres,listRepeticionCaracteres, listFrecuencias, listLogaritmmos, listFrecueciaxLogaritmo))
+    porcentajeFrecuencia=frecuenciaTotal*100
     resultado = math.ceil(entropia)
     
     return{
         'tabla_frecuencias': tabla_frecuencias,
+        'frecuenciaTotal':porcentajeFrecuencia,
+        'totalSumatoria': totalSumatoria,
         'entropia': resultado
     }
     
