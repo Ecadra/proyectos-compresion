63739084a56cec3844e4f60d9ba2cda3af5ec676
Ecadra <edenrulezzz@gmail.com>
2025-04-20 22:47:12 -0600

    Corrección de diccionario innecesario en home

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'core/home.html', {
-        'apps': [
-            {'name': 'Entropia', 'url': 'entropia:index'},
-            {'name': 'Estadistico', 'url': 'estadistico:index'},
-            {'name': 'Huffman', 'url': 'huffman:index'},
-            {'name': 'Aritmetica', 'url': 'aritmetica:index'},
-        ]
-    })
+    return render(request, 'core/home.html')
